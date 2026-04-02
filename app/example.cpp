ffc8e9d40b40db798512d52ee209313ae8a48801
Pravesh Soni <elvez.brachonzei@gmail.com>
2026-04-02 08:32:20 +0530

    minimal

--- app/example.cpp
+++ app/example.cpp
@@ -1,109 +1,108 @@
-#include <vdo-error.h>
-#include <vdo-stream.h>
-#include <vdo-channel.h>
+/**
+ * Copyright (C) 2020 Axis Communications AB, Lund, Sweden
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-#include <cerrno>
-#include <cstdint>
-#include <cstdlib>
-
-#include <poll.h>
-#include <unistd.h>
-
-#include <chrono>
+#include <stdlib.h>
 #include <syslog.h>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/video.hpp>
 
-static bool
-example_detect_sensors(GError** error)
-{
-    g_autoptr(VdoMap) info = vdo_channel_get_info(VDO_CHANNEL_GLOBAL, error);
-    if (!info)
-        return false;
+#include "imgprovider.h"
 
-    auto sensor_count = vdo_map_get_uint32(info, "input.count", 0);
-    syslog(LOG_INFO, "Sensors: %u", sensor_count);
+using namespace cv;
 
-    return true;
-}
+int main(int argc, char* argv[]) {
+  openlog("opencv_app", LOG_PID|LOG_CONS, LOG_USER);
+  syslog(LOG_INFO, "Running OpenCV example with VDO as video source");
+  ImgProvider_t* provider = NULL;
 
-static bool
-example_detect_resolution(VdoStream* stream, GError** error)
-{
-    g_autoptr(VdoMap) info = vdo_stream_get_info(stream, error);
-    if (!info)
-        return false;
+  // The desired width and height of the BGR frame
+  unsigned int width = 1024;
+  unsigned int height = 576;
 
-    auto w = vdo_map_get_uint32(info, "width",  0u);
-    auto h = vdo_map_get_uint32(info, "height", 0u);
-    syslog(LOG_INFO, "Resolution: %ux%u", w, h);
+  // // chooseStreamResolution gets the least resource intensive stream
+  // // that exceeds or equals the desired resolution specified above
+  // unsigned int streamWidth = 0;
+  // unsigned int streamHeight = 0;
+  // if (!chooseStreamResolution(width, height, &streamWidth,
+  //                             &streamHeight)) {
+  //     syslog(LOG_ERR, "%s: Failed choosing stream resolution", __func__);
+  //     exit(1);
+  // }
 
-    return true;
-}
+  syslog(LOG_INFO, "Creating VDO image provider and creating stream %d x %d",
+          width, height);
+  provider = createImgProvider(width, height, 2, VDO_FORMAT_RGBA);
+  if (!provider) {
+    syslog(LOG_ERR, "%s: Failed to create ImgProvider", __func__);
+    exit(2);
+  }
 
-int main()
-{
-    openlog("vdo_example", LOG_PID | LOG_CONS, LOG_USER);
+  syslog(LOG_INFO, "Start fetching video frames from VDO");
+  if (!startFrameFetch(provider)) {
+    syslog(LOG_ERR, "%s: Failed to fetch frames from VDO", __func__);
+    exit(3);
+  }
 
-    g_autoptr(GError) error = nullptr;
+  // Create the background subtractor
+  Ptr<BackgroundSubtractorMOG2> bgsub = createBackgroundSubtractorMOG2();
 
-    auto failed = [&error] {
-        if (vdo_error_is_expected(&error)) {
-            syslog(LOG_INFO, "info: %s", error->message);
-            return EXIT_SUCCESS;
-        } else {
-            syslog(LOG_ERR, "error: %s", error->message);
-            return EXIT_FAILURE;
-        }
-    };
+  // Create the filtering element. Its size influences what is considered
+  // noise, with a bigger size corresponding to more denoising
+  Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(9, 9));
 
-    if (!example_detect_sensors(&error))
-        return failed();
+  // Create OpenCV Mats for the camera frame (nv12), the converted frame (bgr)
+  // and the foreground frame that is outputted by the background subtractor
+  Mat bgra_mat = Mat(height, width, CV_8UC4);
+  // Mat nv12_mat = Mat(height * 3 / 2, width, CV_8UC1);
+  Mat fg;
 
-    g_autoptr(VdoMap) settings = vdo_map_new();
-
-    vdo_map_set_uint32(settings, "channel", 1u);
-    vdo_map_set_uint32(settings, "format", VDO_FORMAT_H264);
-    vdo_map_set_boolean(settings, "socket.blocking", false);
-
-    g_autoptr(VdoStream) stream = vdo_stream_new(settings, nullptr, &error);
-    if (!stream)
-        return failed();
-
-    if (!example_detect_resolution(stream, &error))
-        return failed();
-
-    int fd = vdo_stream_get_fd(stream, &error);
-    if (fd < 0)
-        return failed();
-
-    pollfd fds = {.fd = fd, .events = POLL_IN};
-
-    if (!vdo_stream_start(stream, &error))
-        return failed();
-
-    for (size_t i = 0zu; i < 10zu;) {
-        int status = TEMP_FAILURE_RETRY(poll(&fds, 1, -1));
-        if (status < 0)
-            return failed();
-
-        g_autoptr(VdoBuffer) buffer = vdo_stream_get_buffer(stream, &error);
-        if (!buffer && g_error_matches(error, VDO_ERROR, VDO_ERROR_NO_DATA))
-            continue;
-
-        if (!buffer)
-            return failed();
-
-        VdoFrame* frame = vdo_buffer_get_frame(buffer);
-
-        auto pts    = vdo_frame_get_timestamp(frame);
-        auto pts_us = std::chrono::microseconds(pts);
-
-        syslog(LOG_INFO, "PTS (us): %lld", (long long)pts_us.count());
-
-        if (!vdo_stream_buffer_unref(stream, &buffer, &error))
-            return failed();
-
-        i += 1;
+  while (true) {
+    // Get the latest NV12 image frame from VDO using the imageprovider
+    VdoBuffer* buf = getLastFrameBlocking(provider);
+    if (!buf) {
+      syslog(LOG_INFO, "No more frames available, exiting");
+      exit(0);
     }
 
-    return EXIT_SUCCESS;
+    // Assign the VDO image buffer to the nv12_mat OpenCV Mat.
+    // This specific Mat is used as it is the one we created for NV12,
+    // which has a different layout than e.g., BGR.
+    bgra_mat.data = static_cast<uint8_t*>(vdo_buffer_get_data(buf));
+
+    // Convert the NV12 data to BGR
+    // cvtColor(bgra_mat, bgra_mat, COLOR_, 3);
+
+    // Perform background subtraction on the bgr image with
+    // learning rate 0.005. The resulting image should have
+    // pixel intensities > 0 only where changes have occurred
+    bgsub->apply(bgra_mat, fg, 0.005);
+
+    // Filter noise from the image with the filtering element
+    morphologyEx(fg, fg, MORPH_OPEN, kernel);
+
+    // We define movement in the image as any pixel being non-zero
+    int nonzero_pixels = countNonZero(fg);
+    if (nonzero_pixels > 0) {
+      syslog(LOG_INFO, "Motion detected: YES");
+    } else {
+      syslog(LOG_INFO, "Motion detected: NO");
+    }
+
+    // Release the VDO frame buffer
+    returnFrame(provider, buf);
+  }
+  return EXIT_SUCCESS;
 }
